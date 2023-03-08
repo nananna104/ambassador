49663c09a9df4c45ee8c849b50d842d968657964
nananna <rladskdb@naver.com>
2023-03-08 12:26:58 +0900

    네번째 작업

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    mainInit();
+    $(document).on('click','a[href="#"]',function(e){ e.preventDefault() }) 
+  })
+
+  function mainInit(){
+    gnb_menu();
+  }
+
+  //메뉴
+  function gnb_menu(){
+
+
+  }
